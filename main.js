f3089b94ef427dd5e98ecdd72bfe4630ff0aaa54
kevAs <kevassiobo@gmail.com>
2021-04-25 12:09:32 +0200

    base

---
+++ main.js
@@ -0,0 +1,10 @@
+1
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+eleves.forEach(e => {
+    console.log("Bonjour " + e);
+});
+//2
+let x = ["Alexis", "Andy", "Charles", "Dorian", "Junior"];
+x.forEach(e => {
+    console.log(e);
+});
